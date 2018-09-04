28ca7e9122b025f297415396cf7412707e53dfdc
btjm123 <btjm123@gmail.com>
2018-09-04 14:15:26 +0800

    Create standard.cpp

---
+++ templates/standard.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+void fastio(){ios_base::sync_with_stdio(false); cin.tie(NULL);}
+
+int main () {
+    fastio();
+    
+    // vi arr;
+    // int n; cin >> n;
+    // for (int i = 0;i<n;i++) {
+    //     int a; cin >> a;
+    //     arr.push_back(a);
+    // }
+    
+    return 0;
+}
